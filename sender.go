041267c1963bc64df3e68f60bcbe974261d6c3cb
Michael Brown <micha3lbrown@gmail.com>
2018-08-28 23:37:00 -0400

    Basic tracer object complete
    - Object keys are now epoch timestamps
    - Object value now contains index and human date time
    - Epoch and human time use same time.Time() object instance

--- sender.go
+++ sender.go
@@ -1,11 +1,20 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"strconv"
+	"time"
 
 	riak "github.com/basho/riak-go-client"
 	util "github.com/basho/taste-of-riak/go/util"
 )
+
+type Tracer struct {
+	Index      int64
+	Epoch_time int64
+	Datetime   time.Time
+}
 
 func main() {
 	riak.EnableDebugLogging = true
@@ -39,25 +48,40 @@ func main() {
 
 	if err = cluster.Start(); err != nil {
 		fmt.Println(err.Error())
-	} else {
-		fmt.Println("ping passed")
 	}
 
 	storeRecord(cluster)
 }
 
 func storeRecord(cluster *riak.Cluster) {
+	now := time.Now()
+	tracer := &Tracer{
+		Index:      1,
+		Epoch_time: now.Unix(),
+		Datetime:   now,
+	}
+
+	t, err := json.Marshal(tracer)
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
 	obj := &riak.Object{
-		ContentType:     "text/plain",
+		ContentType:     "application/json",
 		Charset:         "utf-8",
 		ContentEncoding: "utf-8",
-		Value:           []byte("riak stored object"),
+		Value:           []byte(t),
 	}
+
+	key := strconv.FormatInt(now.Unix(), 10)
+	fmt.Println(key)
 
 	cmd, err := riak.NewStoreValueCommandBuilder().
 		WithBucketType("default").
 		WithBucket("test").
-		WithKey("riakobj1").
+		WithKey(key).
 		WithContent(obj).
 		Build()
 
@@ -71,10 +95,6 @@ func storeRecord(cluster *riak.Cluster) {
 		return
 	}
 
-	svc := cmd.(*riak.StoreValueCommand)
-	rsp := svc.Response
-	fmt.Println(svc)
-	fmt.Println(rsp)
-	fmt.Println(rsp.VClock)
-
+	// svc := cmd.(*riak.StoreValueCommand)
+	// rsp := svc.Response
 }
